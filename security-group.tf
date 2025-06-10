993d549de37c4fa16fdd8200404c5dca81826f9c
alokaneunice <alokaneunice@outlook.com>
2025-06-10 12:00:07 +0100

    second commit

--- security-group.tf
+++ security-group.tf
@@ -14,6 +14,12 @@ resource "aws_security_group" "allow_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
